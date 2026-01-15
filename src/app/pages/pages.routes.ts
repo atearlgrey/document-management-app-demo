e2631868f5160610a14a9160c7b1e0022c1833a6
buiquanghieu2910 <bui.quang.hieu2910@gmail.com>
2026-01-15 20:48:06 +0700

    commit "Trung tâm HCC"

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
+import {
+    PublicServiceCenterComponent
+} from '@/pages/ids-public-service-center/public-service-center/public-service-center-component';
+import {
+    AdministrativeProceduresIndexComponent
+} from '@/pages/ids-public-service-center/administrative-procedures-index/administrative-procedures-index-component';
 
 export default [
     { path: 'documentation', component: Documentation },
     { path: 'crud', component: Crud },
     { path: 'empty', component: Empty },
+    { path: 'public-service-center', component: PublicServiceCenterComponent },
+    { path: 'public-service-center/administrative-procedures-index', component: AdministrativeProceduresIndexComponent },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
